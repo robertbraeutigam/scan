87881b5106d6f48658a2cb1b3b1de89b5dfc3015
Robert Brautigam <robert.brautigam@prewave.ai>
2023-06-02 12:15:39 +0200

    Add frame stuff

--- java/client/src/main/java/com/vanillasource/scan/client/network/frame/FrameNetworkListener.java
+++ java/client/src/main/java/com/vanillasource/scan/client/network/frame/FrameNetworkListener.java
@@ -6,16 +6,13 @@
 
 package com.vanillasource.scan.client.network.frame;
 
+import java.util.concurrent.CompletableFuture;
 import com.vanillasource.scan.client.network.PeerAddress;
 import com.vanillasource.scan.client.network.Role;
-import java.util.concurrent.CompletableFuture;
 
-public interface FrameNetwork {
-   CompletableFuture<Void> identityQuery(int queryId, PeerAddress[] addresses);
-
-   CompletableFuture<Void> identityAnnouncement(PeerAddress[] addresses);
-
-   CompletableFuture<Void> close();
+public interface FrameNetworkListener {
+   void receiveAnnouncement(PeerAddress address);
 
    CompletableFuture<FramePeer> connect(PeerAddress address, Role role, FramePeer initiator);
 }
+
