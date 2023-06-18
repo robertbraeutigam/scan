5dafe200451d89034a11ab1a7ce68d2c2dd9dd8e
Robert Brautigam <robert.brautigam@prewave.ai>
2023-06-18 19:28:33 +0200

    Add BitMaskMessageIds and some tests

--- java/client/src/test/java/com/vanillasource/scan/client/network/frame/direct/BitMaskMessageIdsTests.java
+++ java/client/src/test/java/com/vanillasource/scan/client/network/frame/direct/BitMaskMessageIdsTests.java
@@ -9,6 +9,7 @@ package com.vanillasource.scan.client.network.frame.direct;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 import org.testng.annotations.BeforeMethod;
+import java.util.concurrent.CompletableFuture;
 
 @Test
 public final class BitMaskMessageIdsTests {
@@ -29,10 +30,31 @@ public final class BitMaskMessageIdsTests {
       assertEquals(id, 10);
    }
 
+   public void testFirstIdIsAvailableAgainAfterReleaseEvenIfOthersAreStillReserved() {
+      ids.reserveId().join();
+      ids.reserveId().join();
+      ids.reserveId().join();
+      ids.releaseId(10);
+
+      int id = ids.reserveId().join();
+
+      assertEquals(id, 10);
+   }
+
    public void testAllIdsAreAvailable() {
       for (int i=0; i<10; i++) {
          assertEquals(ids.reserveId().join().intValue(), i+10);
       }
+   }
+
+   public void testOverflowIdBlocks() {
+      for (int i=0; i<10; i++) {
+         ids.reserveId().join();
+      }
+      
+      CompletableFuture<Integer> id = ids.reserveId();
+
+      assertFalse(id.isDone());
    }
 
    @BeforeMethod
