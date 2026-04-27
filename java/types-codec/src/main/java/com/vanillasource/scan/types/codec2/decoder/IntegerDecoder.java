2e8741916ea69692b4685b9d0f03a6bcd08f8111
Robert Brautigam <brautigam.robert.gyula@gmail.com>
2026-04-27 22:54:58 +0200

    Rename to IntegerDecoder

--- java/types-codec/src/main/java/com/vanillasource/scan/types/codec2/decoder/IntegerDecoder.java
+++ java/types-codec/src/main/java/com/vanillasource/scan/types/codec2/decoder/IntegerDecoder.java
@@ -1,17 +1,18 @@
 package com.vanillasource.scan.types.codec2.decoder;
 
 import com.vanillasource.scan.types.codec2.BitReader;
-import com.vanillasource.scan.types.codec2.DecodingEvent;
 import com.vanillasource.scan.types.codec2.DecodingEventHandler;
 import com.vanillasource.scan.types.codec2.ValueDecoder;
 
 /** Reads a fixed-width signed big-endian integer with sign extension. */
-final class SignedIntegerDecoder implements ValueDecoder {
+final class IntegerDecoder implements ValueDecoder {
     private int remainingBytes;
+    private final boolean signed;
     private long currentValue = 0;
 
-    SignedIntegerDecoder(int byteSize) {
+    IntegerDecoder(int byteSize, boolean signed) {
         this.remainingBytes = byteSize;
+        this.signed=signed;
     }
 
     @Override
